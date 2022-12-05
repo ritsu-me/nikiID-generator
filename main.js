e6275935ab9626e7cceb594c8c2f4edc61dfeffa
Ritsu.me <mail@ritsu-me.com>
2022-12-05 10:22:01 +0900

    created: main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const generateRandomString = (num) => {
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@-_+*\^~!#$%&";
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < num; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }return result;
+}
+randomStr = generateRandomString(128);
+console.log(randomStr);
